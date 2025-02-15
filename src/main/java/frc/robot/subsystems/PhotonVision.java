456b91844effa7645fee6d13b1639bb07ecccc34
Gloc0de <126303154+Gloc0de@users.noreply.github.com>
2025-02-15 09:17:23 -0800

    Photon vishion

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -3,13 +3,15 @@ package frc.robot.subsystems;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class PhotonVision extends SubsystemBase{
+public class PhotonVision extends SubsystemBase {
     PhotonCamera camera = new PhotonCamera("PhotonCamera");
-    boolean hasTargets = 
-public PhotonPipelineResult getLatestTags(){
-    return camera.getLatestResult();
 
+    public PhotonVision() {
+        // camera.subsystemDriverMode(true, driverMode);
+        PortForwarder.add(5800, "photonvision.local", 5800);
     }
-} 
+
+}
