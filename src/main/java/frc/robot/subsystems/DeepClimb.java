8612d6aace149edce59a29f4d04b0c069c2fb283
Gloc0de <YOUR_EMAIL>
2025-02-23 14:15:59 -0800

    Starting new branch

--- src/main/java/frc/robot/subsystems/DeepClimb.java
+++ src/main/java/frc/robot/subsystems/DeepClimb.java
@@ -1,10 +1,14 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import com.revrobotics.spark.SparkLowLevel.MotorType;
-// import com.revrobotics.spark.SparkMax;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
-// public class DeepClimb extends SubsystemBase {
-//   private final SparkMax climbLeft = new SparkMax(0, MotorType.kBrushless);
-//   private final SparkMax climbRight = new SparkMax(0, MotorType.kBrushless);
-// }
+public class DeepClimb extends SubsystemBase {
+    private final SparkMax climbLeft = new SparkMax(0, MotorType.kBrushless);
+    private final SparkMax climbRight = new SparkMax(0, MotorType.kBrushless);
+    private final SparkMaxConfig climbLeftConfig = new SparkMaxConfig();
+    private final SparkMaxConfig climbRightConfig = new SparkMaxConfig();
+}
