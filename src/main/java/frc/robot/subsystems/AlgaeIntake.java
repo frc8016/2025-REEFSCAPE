e985b3b4366f760d163b0fa8199917e647f48706
Gloc0de <YOUR_EMAIL>
2025-02-24 12:09:00 -0800

    algae mechinise fixed

--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -9,11 +9,11 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import java.security.AlgorithmConstraints;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -57,8 +57,6 @@ public class AlgaeIntake extends SubsystemBase {
                                 .reverseSoftLimit(AlgaeIntakeConstants.ALGEAINTAKE_REVERSE_SOFTLIMIT)
                                 .reverseSoftLimitEnabled(true);
 
-                m_algaeMotor.configure(m_algaeMotorconfig, ResetMode.kResetSafeParameters,
-                                PersistMode.kPersistParameters);
         }
 
         // lets driverstation set a position
