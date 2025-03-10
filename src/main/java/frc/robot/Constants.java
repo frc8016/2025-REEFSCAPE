860282a78d8e123b7b9a413c5c8e0cecf85b32de
Gloc0de <YOUR_EMAIL>
2025-03-10 16:48:14 -0700

    edited Alaeintake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -88,6 +88,7 @@ public final class Constants {
         public static final Angle ALLOWED_SETPOINT_ERROR = Degrees.of(1);
         public static final double ALGEAINTAKE_FORWORD_SOFTLIMIT = 1;
         public static final double ALGEAINTAKE_REVERSE_SOFTLIMIT = 0;
+        public static final int MAX_CURRENT_LIMIT = 50;
 
         public static class DeepClimbConstants {
             public static final double DEGREE_PER_REVOLUTION = 360;
--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -19,6 +19,7 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -29,55 +30,43 @@ import frc.robot.Constants.ElevatorConstants;
 
 //test
 public class AlgaeIntake extends SubsystemBase {
-        // v- needed
-        private final SparkMax m_algaeMotor = new SparkMax(18, MotorType.kBrushless); // double check id 
-        private final SparkMaxConfig m_algaeMotorconfig = new SparkMaxConfig();
-        private final SparkClosedLoopController m_algaeClosedLoopController = m_algaeMotor.getClosedLoopController();
-        private final DigitalInput m_beamBreak = new DigitalInput(0);
+    // v- needed
+    private final SparkMax m_algaeMotor = new SparkMax(19, MotorType.kBrushless); // double check id
+    private final SparkMaxConfig m_algaeMotorconfig = new SparkMaxConfig();
+    private final SparkClosedLoopController m_algaeClosedLoopController = m_algaeMotor.getClosedLoopController();
+    private final DigitalInput m_beamBreak = new DigitalInput(0);
 
-        public AlgaeIntake() {
-                // converts Rotations values to degree values
-                m_algaeMotorconfig.encoder
-                                .positionConversionFactor(AlgaeIntakeConstants.DEGREE_PER_REVOLUTION)
-                                .velocityConversionFactor(AlgaeIntakeConstants.VELOCITY_CONVERSHION_FACTOR);
-                // configs the pid loops
-                m_algaeMotorconfig.closedLoop
-                                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                .p(AlgaeIntakeConstants.P_VALUE, ClosedLoopSlot.kSlot0)
-                                .i(AlgaeIntakeConstants.I_VALUE, ClosedLoopSlot.kSlot0)
-                                .d(AlgaeIntakeConstants.D_VALUE, ClosedLoopSlot.kSlot0)
-                                .outputRange(AlgaeIntakeConstants.OUTPUTRANGE_MIN_VALUE,
-                                                AlgaeIntakeConstants.OUTPUTRANGE_MAX_VALUE);
-                // configs maxMotion
-                m_algaeMotorconfig.closedLoop.maxMotion
-                                .maxVelocity(AlgaeIntakeConstants.MAX_VEL)
-                                .maxAcceleration(AlgaeIntakeConstants.MAX_ACCELERATION)
-                                .positionMode(MAXMotionPositionMode.kMAXMotionTrapezoidal)
-                                .allowedClosedLoopError(AlgaeIntakeConstants.ALLOWED_SETPOINT_ERROR.in(Degrees));
-                // sets some softlimits
-                m_algaeMotorconfig.softLimit
-                                .forwardSoftLimit(AlgaeIntakeConstants.ALGEAINTAKE_FORWORD_SOFTLIMIT)
-                                .forwardSoftLimitEnabled(true)
-                                .reverseSoftLimit(AlgaeIntakeConstants.ALGEAINTAKE_REVERSE_SOFTLIMIT)
-                                .reverseSoftLimitEnabled(true);
+    public AlgaeIntake() {
 
-        }
+        m_algaeMotorconfig
+                .inverted(true)
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(AlgaeIntakeConstants.MAX_CURRENT_LIMIT);
+        // sets some softlimits
+        m_algaeMotorconfig.softLimit
+                .forwardSoftLimit(AlgaeIntakeConstants.ALGEAINTAKE_FORWORD_SOFTLIMIT)
+                .forwardSoftLimitEnabled(true)
+                .reverseSoftLimit(AlgaeIntakeConstants.ALGEAINTAKE_REVERSE_SOFTLIMIT)
+                .reverseSoftLimitEnabled(true);
 
-        // lets driverstation set a position
-        public void setPosition(double position) {
-                m_algaeClosedLoopController.setReference(position, ControlType.kMAXMotionPositionControl,
-                                ClosedLoopSlot.kSlot0);
-        }
+    }
 
-        // makes a command we can use
-        public Command goToSetPointCommand(double position) {
-                return this.runOnce(() -> this.setPosition(position));
+    // lets driverstation set a position
+    public void setPosition(double position) {
+        m_algaeClosedLoopController.setReference(position, ControlType.kMAXMotionPositionControl,
+                ClosedLoopSlot.kSlot0);
+    }
 
-        }
-@Override
-public void periodic(){
+    // makes a command we can use
+    public Command goToSetPointCommand(double position) {
+        return this.runOnce(() -> this.setPosition(position));
+
+    }
+
+    @Override
+    public void periodic() {
         SmartDashboard.putNumber("Algea Position", m_algaeMotor.getEncoder().getPosition());
         SmartDashboard.putNumber("Algae Velocity", m_algaeMotor.getEncoder().getVelocity());
-}
+    }
 
 }
