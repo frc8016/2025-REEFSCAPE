c94587f61868b027a77348663398eef4af5d6aa7
Gloc0de <YOUR_EMAIL>
2025-03-21 19:52:27 -0700

    fixed robots mass in path planner

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -17,6 +18,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
+        CameraServer.startAutomaticCapture();
         m_robotContainer = new RobotContainer();
         m_Vision = new Vision();
     }
