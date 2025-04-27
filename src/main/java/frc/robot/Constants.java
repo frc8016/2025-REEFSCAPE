adf3f9848ecef6f78e4cda99691c376da9f0f3bf
Jeremiah Welch <jeremiahwelch88@gmail.com>
2025-04-27 14:05:29 -0700

    fixed constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,7 +51,7 @@ public final class Constants {
     }
 
     public static class SetPointConstants {
-        public static double LEVEL4 = (16);
+        public static double LEVEL4 = (16.0);
         public static double LEVEL3 = (9.0);
         public static double LEVEL2 = (4.5);
         public static double TROUGH = (2.0);
