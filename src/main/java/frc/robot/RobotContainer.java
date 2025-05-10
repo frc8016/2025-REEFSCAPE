6e153f1102e3e85d7e3039f4c4c527a1a3691bfe
Jeremiah Welch <jeremiahwelch88@gmail.com>
2025-05-10 12:06:41 -0700

    rebound more controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -99,10 +99,13 @@ public class RobotContainer {
                         -m_swerveController.getLeftX()))));
 
         // reset the field-centric heading on left bumper press
-        m_swerveController.leftBumper().onTrue(m_Drivetrain.runOnce(() -> m_Drivetrain.seedFieldCentric()));
+        // m_swerveController.leftBumper().onTrue(m_Drivetrain.runOnce(() -> m_Drivetrain.seedFieldCentric()));
+
+        m_swerveController.leftTrigger().whileTrue(new PathfindToScore(m_Drivetrain, Direction.LEFT).createPathfindToScoreCommand());
+        m_swerveController.rightTrigger().whileTrue(new PathfindToScore(m_Drivetrain, Direction.RIGHT).createPathfindToScoreCommand());
 
 /*Deep climb code */
-        m_swerveController.rightTrigger().whileTrue(
+        m_swerveController.rightBumper().whileTrue(
                 new SequentialCommandGroup(
                         new StartEndCommand(
                        () -> m_DeepClimb.run(.5), 
@@ -114,7 +117,7 @@ public class RobotContainer {
                 
 
 
-        m_swerveController.leftTrigger().whileTrue(
+        m_swerveController.leftBumper().whileTrue(
                 new SequentialCommandGroup(
                         new StartEndCommand(
                         () -> m_DeepClimb.release(-.5 ), 
